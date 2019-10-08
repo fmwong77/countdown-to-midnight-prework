e8c6d1fab3f9cbdb6543f059a4253116235d1ea7
Mei Wong <fongmeiw@gmail.com>
2019-10-08 17:23:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,10 @@
 #write your code here
 
-def countdown(counter)
+def countdown(number)
   i = 0
-  while i < counter do
+  while i < number do
     puts "#{number} SECOND(S)!"
   end
+  
+  puts "HAPPY NEW YEAR!"
 end
